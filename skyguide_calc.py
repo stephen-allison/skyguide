157dc8e327c42b47245edb52dd0db9dea033074a
Stephen Allison <Stephen@git>
2012-07-08 22:57:33 +0100

    add function to return dictionary of data for planets

--- skyguide_calc.py
+++ skyguide_calc.py
@@ -14,6 +14,16 @@ def compute_properties(bodies, observer):
     for body in bodies:
         body.compute(observer)
 
+def get_solar_system_body_properties():
+    bodies = get_solar_system_objects()
+    obs = epm.city("London")
+    compute_properties(bodies, obs)
+    data = {'date':str(obs.date)}
+    for body in bodies:
+        body_data = {n: str(getattr(body, n)) for n in ['name', 'alt', 'az', 'mag', 'radius','rise_time', 'transit_time', 'set_time']}
+        data[body.name] = body_data
+    return data
+
 def test():
     bodies = get_solar_system_objects()
     obs = epm.city("London")
