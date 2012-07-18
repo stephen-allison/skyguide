b98dc372525707b1ba1e1f412a61c704c602bf77
Stephen Allison <Stephen@git>
2012-07-18 23:00:37 +0100

    convert python datetimes to pyephem dates

--- planner/planner.py
+++ planner/planner.py
@@ -1,6 +1,7 @@
 # module planner
 # allows hour-by-hour, night-by-night observability of PyEphem Bodies to
 # be calculated
+import ephem
 from datetime import timedelta
 
 def date_sequence(start,periods=365):
@@ -10,11 +11,10 @@ def hour_sequence(start,periods=24):
     start = start.replace(hour=12)
     return t_sequence(start, timedelta(hours=1), periods)
 
-
 def t_sequence(start,delta,periods):
-    seq = []
-    for i in range(periods):
-        seq.append(start)
-        start = start + delta
-    return seq
+    return [ start+(i*delta) for i in range(periods)]
 
+def to_ephem_date(datetime):
+    tup = datetime.timetuple()
+    ephem_date = ephem.Date(tup[:5])
+    return ephem_date
--- planner/planner_test.py
+++ planner/planner_test.py
@@ -3,6 +3,7 @@
 from nose.tools import eq_, ok_
 from datetime import date, timedelta, datetime
 import planner
+import ephem
 
 def test_date_sequence():
     periods = 365
@@ -23,3 +24,8 @@ def test_hour_sequence():
     eq_(total_delta,delta*23)
     eq_(r[0].hour,12)
 
+def test_to_ephem_date():
+    dt = datetime(2012,1,1,22,30)
+    e_dt = planner.to_ephem_date(dt)
+    ok_(isinstance(e_dt, ephem.Date))
+    eq_(e_dt.tuple(), (2012,1,1,22,30,0))
