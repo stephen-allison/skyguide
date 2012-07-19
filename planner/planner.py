b60e9d304da46e65d8138b72b50eedd0fa4366be
Stephen Allison <Stephen@git>
2012-07-19 22:50:44 +0100

    compute proeprties for a single list of times (ie a day)

--- planner/planner.py
+++ planner/planner.py
@@ -15,6 +15,19 @@ def t_sequence(start,delta,periods):
     return [ start+(i*delta) for i in range(periods)]
 
 def to_ephem_date(datetime):
+    """Converts a standard python datetime.datetime to a pyephem date"""
     tup = datetime.timetuple()
     ephem_date = ephem.Date(tup[:5])
     return ephem_date
+
+def create_plan_day(observer, bodies, times, entry_builder):
+    results = []
+    for time in times:
+        observer.date = time
+        for body in bodies:
+            body.compute(observer)
+            results.append(entry_builder(body))
+    return results
+
+def get_body_properties(computed_body):
+    return {'az':computed_body.az}
--- planner/planner_test.py
+++ planner/planner_test.py
@@ -4,6 +4,7 @@ from nose.tools import eq_, ok_
 from datetime import date, timedelta, datetime
 import planner
 import ephem
+import mock
 
 def test_date_sequence():
     periods = 365
@@ -29,3 +30,30 @@ def test_to_ephem_date():
     e_dt = planner.to_ephem_date(dt)
     ok_(isinstance(e_dt, ephem.Date))
     eq_(e_dt.tuple(), (2012,1,1,22,30,0))
+
+
+def test_properties_computed():
+    start_time = datetime(2012,1,1)
+    times = planner.hour_sequence(start_time)
+    observer = ephem.city('London')
+    body = ephem.Sun()
+
+    body.compute = mock.MagicMock(name='compute')
+    builder = mock.MagicMock(name='builder')
+    builder.return_value = {}
+
+    plan = planner.create_plan_day(observer, [body], times, builder)
+
+    expected_compute_calls = [mock.call(observer)]*24
+    compute_calls = body.compute.mock_calls
+
+    expected_build_calls = [mock.call(body)]*24
+    build_calls = builder.mock_calls
+    eq_(len(plan), 24)
+
+def test_get_body_properties():
+    body = ephem.Sun();
+    obs = ephem.city('London')
+    body.compute(obs)
+    props = planner.get_body_properties(body)
+    ok_(props['az'] != None)
